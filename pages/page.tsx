d39664540eadd36d356658176e9243d26843fc8e
Ryo098 <ryo0812eng@gmail.com>
2022-08-08 17:12:18 +0900

    Add page.tsx How to useRouter

--- pages/page.tsx
+++ pages/page.tsx
@@ -1,7 +1,15 @@
-const sample = () => {
-  return (
-    <></>
-  )
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+
+const Page = () => {
+  const router = useRouter()
+
+  // コメントアウトを解除すると/userouterに移動するようになる
+  // useEffect( ()=> {
+  //   router.push("/userouter")
+  // })
+
+  return <span>{router.pathname}</span>
 }
 
-export default sample
+export default Page
