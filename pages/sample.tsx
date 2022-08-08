b59c7e3029f85e1e7e57beb100aedc61f36d9af6
Ryo098 <ryo0812eng@gmail.com>
2022-08-08 15:59:54 +0900

    Create template sample.tsx

--- pages/sample.tsx
+++ pages/sample.tsx
@@ -1,5 +1,24 @@
-function Sample() {
-  return <span>sample</span>
-}
+import { NextPage } from "next"
+import Head from "next/head"
 
-export default Sample
+//TODO:ページコンポーネントのpropsの型定義
+type SSGProps = {}
+
+
+//SSGページ
+const SSG: NextPage <SSGProps> = () => {
+  return (
+    <div>
+      <Head>
+      <title>Static Site Generation</title>
+      <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <p>
+          このページは静的サイト生成によってビルド時に生成されたページです。
+        </p>
+      </main>
+    </div>
+  )
+}
+export default SSG
